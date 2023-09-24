838ef5f623914751926ebb29c7e9dc361cd1bcc9
Crowdbotics <team@crowdbotics.com>
2023-09-24 22:26:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "global-egg-bank-43545",
+  url: "https://global-egg-bank-43545.botics.co",
+  api: "https://global-egg-bank-43545.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
